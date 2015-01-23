67b939f1bc00728a2099519352b8261d779ce82a
Donne Martin <donne.martin@gmail.com>
2015-01-23 16:09:17 -0500

    Added snippet that tests for whether an object is of a particular instance

---
+++ tests/test_util.py
@@ -0,0 +1,30 @@
+from nose.tools import assert_equal
+
+
+class TestUtil():
+
+    @classmethod
+    def setup_class(cls):
+        """This method is run once for each class before any tests are run"""
+
+    @classmethod
+    def teardown_class(cls):
+        """This method is run once for each class _after_ all tests are run"""
+
+    def setUp(self):
+        """This method is run once before _each_ test method is executed"""
+
+    def teardown(self):
+        """This method is run once after _each_ test method is executed"""
+
+    def test_isinstance(self):
+        assert_equal(isinstance(7, (int)), True)
+        assert_equal(isinstance(7.5, (int, float)), True)
+        assert_equal(isinstance('foo', (int, float, str)), True)
+
+def main():
+    test = TestUtil()
+    test.test_isinstance()
+
+if __name__ == "__main__":
+    main()
