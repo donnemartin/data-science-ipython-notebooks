125f5c0c81d34a46d5d33301779aa246117bfbbb
Donne Martin <donne.martin@gmail.com>
2015-01-25 06:07:04 -0500

    Renamed Util class to TypeUtil to be more descriptive.

--- core/tests/test_type_util.py
+++ core/tests/test_type_util.py
@@ -1,13 +1,13 @@
 from nose.tools import assert_equal
-from pydatasnippets.core.type_util import Util
+from pydatasnippets.core.type_util import TypeUtil
 
 
 class TestUtil():
 
     def test_is_iterable(self):
-        assert_equal(Util.is_iterable('foo'), True)
-        assert_equal(Util.is_iterable(7), False)
+        assert_equal(TypeUtil.is_iterable('foo'), True)
+        assert_equal(TypeUtil.is_iterable(7), False)
 
     def test_convert_to_list(self):
-        assert_equal(isinstance(Util.convert_to_list('foo'), list), True)
-        assert_equal(isinstance(Util.convert_to_list(7), list), False)
+        assert_equal(isinstance(TypeUtil.convert_to_list('foo'), list), True)
+        assert_equal(isinstance(TypeUtil.convert_to_list(7), list), False)
--- core/type_util.py
+++ core/type_util.py
@@ -1,4 +1,4 @@
-class Util:
+class TypeUtil:
 
     @classmethod
     def is_iterable(cls, obj):
